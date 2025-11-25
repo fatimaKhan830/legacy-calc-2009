b2791a39b60d3743922b4d14ba5073089e1b2c43
FATIMA KHAN <fkfatimakhan17@gmail.com>
2025-11-25 13:57:32 +0500

    WE HAVE PUSHED THE MAIN FILE

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,21 @@
-#include<iostream>
-
+#include <iostream>
+#include "Loan.h"
 using namespace std;
 
-int main(){
-    double loan_amount,interest_rate,number_of_years,total_amount,monthly_amount;
+int main() {
+    Loan loan;
 
-	cout<<"Enter the loan amount: ";
-	cin>>loan_amount;
-	cout<<endl;
-	cout<<"Enter the interest rate: ";
-	cin>>interest_rate;
-	cout<<endl;
-	cout<<"The number of years: ";
-	cin>>number_of_years;
-	cout<<endl;
+    loan.readLoanAmount();
+    loan.readInterestRate();
+    loan.readNumberOfYears();
 
-	total_amount=(number_of_years*loan_amount)+(number_of_years*loan_amount*(interest_rate/100.00));
-	monthly_amount=total_amount/(number_of_years*12);
+    long double total = loan.calculateTotalAmount();
+    long double monthly = loan.calculateMonthlyAmount(total);
 
-	cout<<"Total amount to be paid: "<<total_amount<<endl;
-	cout<<"Total interest: "<<total_amount-(number_of_years*loan_amount)<<endl;
-	cout<<"Monthly amount to be paid: "<<monthly_amount<<endl;
+    cout << "Total amount to be paid: " << total << endl;
+    cout << "Total interest: " 
+         << total - (loan.getLoanAmount() * loan.getYears()) << endl;
+    cout << "Monthly payment: " << monthly << endl;
 
-	return 0;
+    return 0;
 }
